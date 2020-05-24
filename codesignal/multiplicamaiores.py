8c668b51e837471f163ffba64fada5630514b7df
jade <jug.guerra@gmail.com>
2020-05-23 23:34:02 -0300

    arrumar isso

--- codesignal/multiplicamaiores.py
+++ codesignal/multiplicamaiores.py
@@ -1,5 +1,7 @@
 #multiplica os maiores adjacentes do array entre si 
 # e retorna o maior produto
+#TODO o resultado do maior número está trabalhando por módulo ao invés de 
+#trabalhar o valor da reta.
 
 def multiplica(inputArray):
     multiply = 0
@@ -10,7 +12,9 @@ def multiplica(inputArray):
         #multiplica os elementos do array
         multiply = inputArray[a] * inputArray[b]
         if multiply > bigger:
-            bigger = multiply        
+            bigger = multiply
+        print(inputArray[a], inputArray[b])  
+        print(multiply)      
         a+=1
         b+=1
     return bigger
